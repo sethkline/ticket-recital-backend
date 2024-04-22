10f55c7ee6dbbe12b738b039918b2203621e3dee
Seth Kline <seth.mkline@gmail.com>
2024-04-21 23:57:24 -0400

    handle errors gracefully

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,29 @@
 const parse = require("pg-connection-string").parse;
 
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
+// Fallback to empty string to prevent errors if not set
+const connectionString = process.env.DATABASE_URL || "";
+
+// Parse the connection string
+const config = parse(connectionString);
+
+if (!config.host || !config.user) {
+  console.error('Invalid or missing DATABASE_URL');
+  process.exit(1); // Exit if essential details are missing
+}
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false,
       },
     },
-      debug: false,
+    debug: false,
   },
 });
