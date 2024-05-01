35e12f43c972dc072807305cc29c4a7261945326
Seth Kline <seth.mkline@gmail.com>
2024-04-30 21:29:05 -0400

    debug info for pupeteer

--- src/api/order/services/pdfService.js
+++ src/api/order/services/pdfService.js
@@ -4,21 +4,26 @@ module.exports = {
     console.log('HTML Content: ', htmlContent);
       const puppeteer = require('puppeteer');
 
-      const browser = await puppeteer.launch({args: ['--disable-web-security']});
+      try {
+        const browser = await puppeteer.launch({args: ['--disable-web-security']});
+        // your code
+
+      // const browser = await puppeteer.launch({args: ['--disable-web-security']});
       const page = await browser.newPage();
 
       page.on('console', message => {
         console.log(`Browser console: ${message.text()}`);
       });
 
-      try {
+
+      // try {
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
         const pdfBuffer = await page.pdf({ format: 'A4',   printBackground: true,   });
         await browser.close();
         return pdfBuffer;
       } catch (error) {
         console.error('Error generating PDF:', error);
-        await browser.close();
+        // await browser.close();
         throw error; // Re-throw the error to handle it further up the call stack if necessary
       }
   },
